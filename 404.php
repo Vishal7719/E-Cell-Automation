6de0b2829d81fc3dbadf06a94283f465e8310761
Vishal Chaudhari <70936532+Vishal7719@users.noreply.github.com>
2023-07-28 16:53:48 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php include "./header.php"; ?>
+
+<div class="container" style="min-height: 600px;">
+    <center>
+        <br><br><br><br><br><br>
+        <h2>
+            Sorry. Your requested page not found.
+        </h2>
+        <br><br><br><br><br><br>
+    </center>
+</div>
+
+<?php include "./footer.php"; ?>
